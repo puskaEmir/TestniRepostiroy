f53d485f8728559f3511c1248b5b3038ac0f0c87
Emir Puska <emir.puska@preventgroup.com>
2021-01-23 12:32:47 +0100

    Drugi commit na LOKALNI repository

--- TestProjectGit/src/one/Executor.java
+++ TestProjectGit/src/one/Executor.java
@@ -2,9 +2,22 @@ package one;
 /**
  * Ovo je klasa koju želim sačuvati u okviru lokalnog 
  * GIT repository.a
+ * <li>
+ * Upravo sam ubacio sve promjene u svom projektu iz UNSTAGED  u STAGED 
+ * tako što sam ih dodao u INDEKS -> ADD to index
+ * <p>
+ * STAGED - fajlovi koje želim commitati ili sačuvati u svom LOKALNO REPOSITORY
+ * <p>
+ * COMMIT -> komandu sam izvršio i snimio sve izmjene u kodu u LOKALNI repository
+ * 
  * @author emir
  *
  */
 public class Executor {
 
+	//Primijetite kad god uradim neku izmjenu u Git STAGING prikazu mi se pojavi taj fajl nad kojim sam radio izmjenu
+	//KAKO SAM UPRAVO MIJENJAO Executor.java ili ovu klasu gdje pišem komentar trebalo bi da mi se ta klasa pojavila
+	public static void main(String[] args) {
+		
+	}
 }
